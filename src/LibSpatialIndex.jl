139297539b1a06108dee3d2e646349dfc3a75e5a
Yeesian Ng <ngyeesian@gmail.com>
2017-12-10 11:56:09 -0500

    add some operations: `insert!`, `intersects`, and `knn`

--- src/LibSpatialIndex.jl
+++ src/LibSpatialIndex.jl
@@ -2,6 +2,115 @@ module LibSpatialIndex
 
     include("capi.jl")
 
-    version() = unsafe_string(SIDX_Version())
+    version() = unsafe_string(C.SIDX_Version())
 
+    mutable struct RTree
+        index::C.IndexH
+        indextype::C.RTIndexType
+        ndim::Integer
+        nitems::Int
+        variant::C.RTIndexVariant
+        storage::C.RTStorageType
+        indexcapacity::Integer
+        leafcapacity::Integer
+        leafpoolcapacity::Integer
+        indexpoolcapacity::Integer
+        regionpoolcapacity::Integer
+        pointpoolcapacity::Integer
+        tightMBR::Bool
+        nearminimumoverlapfactor::Integer
+        fillfactor::Real
+        splitdistributionfactor::Real
+        reinsertfactor::Real
+
+        function RTree(ndim::Integer;
+                indextype::C.RTIndexType = C.RT_RTree,
+                variant::C.RTIndexVariant = C.RT_Star,
+                storage::C.RTStorageType = C.RT_Memory,
+                indexcapacity::Integer = 100,
+                leafcapacity::Integer = 100,
+                leafpoolcapacity::Integer = 100,
+                indexpoolcapacity::Integer = 100,
+                regionpoolcapacity::Integer = 1000,
+                pointpoolcapacity::Integer = 500,
+                tightMBR::Bool = true,
+                nearminimumoverlapfactor::Integer = 32,
+                fillfactor::Real = 0.7,
+                splitdistributionfactor::Real = 0.4,
+                reinsertfactor::Real = 0.3
+            )
+            p = C.IndexProperty_Create()
+            C.IndexProperty_SetIndexType(p, indextype)
+            C.IndexProperty_SetDimension(p, UInt32(ndim))
+            C.IndexProperty_SetIndexVariant(p, variant)
+            C.IndexProperty_SetIndexStorage(p, storage)
+            C.IndexProperty_SetIndexCapacity(p, UInt32(indexcapacity))
+            C.IndexProperty_SetLeafCapacity(p, UInt32(leafcapacity))
+            C.IndexProperty_SetLeafPoolCapacity(p, UInt32(leafpoolcapacity))
+            C.IndexProperty_SetIndexPoolCapacity(p, UInt32(indexpoolcapacity))
+            C.IndexProperty_SetRegionPoolCapacity(p, UInt32(regionpoolcapacity))
+            C.IndexProperty_SetPointPoolCapacity(p, UInt32(pointpoolcapacity))
+            # IndexProperty_SetBufferingCapacity(iprop::IndexPropertyH, value::UInt32)
+            C.IndexProperty_SetEnsureTightMBRs(p, UInt32(tightMBR))
+            # IndexProperty_SetOverwrite(iprop::IndexPropertyH, value::UInt32)
+            C.IndexProperty_SetNearMinimumOverlapFactor(p, UInt32(nearminimumoverlapfactor))
+            # IndexProperty_SetWriteThrough(iprop::IndexPropertyH, value::UInt32)
+            C.IndexProperty_SetFillFactor(p, fillfactor)
+            C.IndexProperty_SetSplitDistributionFactor(p, splitdistributionfactor)
+            # IndexProperty_SetTPRHorizon(iprop::IndexPropertyH, value::Cdouble)
+            C.IndexProperty_SetReinsertFactor(p, reinsertfactor)
+            # IndexProperty_SetFileName(iprop::IndexPropertyH, value)
+            # IndexProperty_SetFileNameExtensionDat(iprop::IndexPropertyH, value)
+            # IndexProperty_SetFileNameExtensionIdx(iprop::IndexPropertyH, value)
+            # IndexProperty_SetCustomStorageCallbacksSize(iprop::IndexPropertyH, value::UInt32)
+            # IndexProperty_SetCustomStorageCallbacks(iprop::IndexPropertyH, value)
+            # IndexProperty_SetIndexID(iprop::IndexPropertyH, value::Int64)
+            index = C.Index_Create(p)
+            Bool(C.Index_IsValid(index)) || error("Invalid index")
+            C.IndexProperty_Destroy(p)
+            rtree = new(index, indextype, ndim, 0, variant, storage, indexcapacity,
+                leafcapacity, leafpoolcapacity, indexpoolcapacity, regionpoolcapacity,
+                pointpoolcapacity, tightMBR, nearminimumoverlapfactor, fillfactor,
+                splitdistributionfactor, reinsertfactor)
+            finalizer(rtree, x -> (C.Index_Destroy(x.index); x.index = C_NULL))
+            rtree
+        end
+    end
+
+    function insert!(rtree::RTree, id::Int, minvalues, maxvalues)
+        @assert length(minvalues) == length(maxvalues)
+        C.Index_InsertData(rtree.index, id, pointer(minvalues),
+            pointer(maxvalues), UInt32(length(minvalues)), Ptr{UInt8}(0), Cint(0)
+        )
+    end
+
+    function intersects(rtree::RTree, minvalues, maxvalues)
+        ndim = length(minvalues)
+        @assert ndim == length(maxvalues)
+        items = Ref{Ptr{Int64}}()
+        nresults = Ref{UInt64}()
+        result = C.Index_Intersects_id(rtree.index, pointer(minvalues),
+            pointer(maxvalues), UInt32(ndim), items, nresults
+        )
+        if result == C.RT_Failure || result == C.RT_Fatal
+            error("Index_Intersects_id: Failed to evaluate")
+        else
+            return unsafe_wrap(Array, items[], nresults[])
+        end
+    end
+
+    function knn(rtree::RTree, minvalues, maxvalues, k::Integer)
+        ndim = length(minvalues)
+        @assert ndim == length(maxvalues)
+        items = Ref{Ptr{Int64}}()
+        nresults = Ref{UInt64}(k)
+        result = C.Index_NearestNeighbors_id(rtree.index, pointer(minvalues),
+            pointer(maxvalues), UInt32(ndim), items, nresults)
+        if result == C.RT_Failure || result == C.RT_Fatal
+            error("Index_NearestNeighbors_id: Failed to evaluate")
+        else
+            return unsafe_wrap(Array, items[], nresults[])
+        end
+    end
+    
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,31 +2,44 @@ using LibSpatialIndex, Base.Test; const SI = LibSpatialIndex
 
 @testset "Simple Tutorial" begin
     # based on https://github.com/libspatialindex/libspatialindex/wiki/Simple-Tutorial
-    SI.version()
-    props = SI.IndexProperty_Create()
-    SI.IndexProperty_SetIndexType(props, SI.RT_RTree)
-    SI.IndexProperty_SetIndexStorage(props, SI.RT_Memory)
-    idx = SI.Index_Create(props)
-    SI.IndexProperty_Destroy(props)
-    @test Bool(SI.Index_IsValid(idx))
+    println("Testing LibSpatialIndex v$(SI.version())")
+    props = SI.C.IndexProperty_Create()
+    SI.C.IndexProperty_SetIndexType(props, SI.C.RT_RTree)
+    SI.C.IndexProperty_SetIndexStorage(props, SI.C.RT_Memory)
+    idx = SI.C.Index_Create(props)
+    SI.C.IndexProperty_Destroy(props)
+    @test Bool(SI.C.Index_IsValid(idx))
 
     # load()
     min = [0.5, 0.5]
     max = [0.5, 0.5]
-    SI.Index_InsertData(idx, 1, min, max, UInt32(2), Ptr{UInt8}(C_NULL), Int32(0))
+    SI.C.Index_InsertData(idx, 1, min, max, UInt32(2), Ptr{UInt8}(C_NULL), Int32(0))
 
     # query()
     min = [0.0, 0.0]
     max = [1.0, 1.0]
     ndims = UInt32(2)
     nresults = Ref{UInt}()
-    SI.Index_Intersects_count(idx, min, max, ndims, nresults)
+    SI.C.Index_Intersects_count(idx, min, max, ndims, nresults)
     @test nresults[] == 1
 
     # bounds()
     pmins = zeros(2)
     pmaxs = zeros(2)
     ndims = Ref{UInt32}()
-    SI.Index_GetBounds(idx, pointer(pmins), pointer(pmaxs), ndims);
+    SI.C.Index_GetBounds(idx, pointer(pmins), pointer(pmaxs), ndims);
     @test ndims[] == 2
+end
+
+@testset "Simple Operations" begin
+    rtree = SI.RTree(2)
+    result = SI.insert!(rtree, 1, [0.,0.], [1.,1.])
+    @test result == SI.C.RT_None
+    result = SI.insert!(rtree, 2, [0.,0.], [2.,2.])
+    @test result == SI.C.RT_None
+    @test SI.intersects(rtree, [0.,0.],[1.,1.]) == [1,2]
+    @test SI.intersects(rtree, [1.5,1.5],[2.,2.]) == [2]
+    @test sort(SI.knn(rtree, [2.,2.],[2.,2.], 1)) == [2]
+    @test sort(SI.knn(rtree, [2.,2.],[2.,2.], 2)) == [1,2]
+    @test sort(SI.knn(rtree, [2.,2.],[2.,2.], 3)) == [1,2]
 end
