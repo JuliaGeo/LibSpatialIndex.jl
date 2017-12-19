e82d0aed051ba821208b6136ae14257379d75e84
Martijn Visser <mgvisser@gmail.com>
2017-12-19 20:02:39 +0100

    use BinDeps to download binary for Windows

---
+++ deps/build.jl
@@ -0,0 +1,17 @@
+using BinDeps
+
+@BinDeps.setup
+
+libspatialindex = library_dependency("libspatialindex", aliases=["spatialindex_c-64"])
+version = v"1.8.5"
+
+if is_windows()
+    url = "http://download.osgeo.org/libspatialindex/"
+    archive = "libspatialindex-$version-win-msvc-2013-x64.zip"
+    uri = URI("http://download.osgeo.org/libspatialindex/$archive")
+    libpath = joinpath(BinDeps.depsdir(libspatialindex), "libspatialindex", "build", "bin")
+    provides(Binaries, uri, libspatialindex,
+        unpacked_dir=".", installed_libpath=libpath, os=:Windows)
+end
+
+@BinDeps.install Dict(:libspatialindex => :libspatialindex)
