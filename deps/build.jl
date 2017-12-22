209bf9fb1545bfd954f6a102c86fc6cd9ebcfc88
Yeesian Ng <ngyeesian@gmail.com>
2017-12-21 20:27:28 -0500

    Provide a build script (#1)
    * use BinDeps to download binary for Windows
    
    * add for homebrew
    
    * update yaml config files
    
    * add Homebrew
    
    * try downloading binaries for unix platforms
    
    * win32 unsupported for now
    
    * typo
    
    * unix installation instructions
    
    * typo
    
    * try again
    
    * list files
    
    * try again
    
    * change library name back, add alias
    
    * use a different installation directory
    
    * add a version checking validation function
    
    * return false instead of error on `SIDX_Version` lookup failure
    
    * interpolate url
    
    * try going through a single build process for all unix platforms
    
    * allow failures on os x for now
    
    suspect it’s due to issues with Homebrew.jl for now.
    
    users should be able to install it themselves for now by running `brew
    install spatialindex`
    
    * remove Homebrew from REQUIRE

---
+++ deps/build.jl
@@ -0,0 +1,50 @@
+using BinDeps
+
+@BinDeps.setup
+
+const version = v"1.8.5"
+
+function version_check(name, handle)
+    fptr = Libdl.dlsym_e(handle, :SIDX_Version)
+    if fptr == C_NULL  # lookup failure
+        return false
+    end
+    versionptr = ccall(fptr, Cstring, ())
+    versionstring = unsafe_string(versionptr)
+    foundversion = convert(VersionNumber, versionstring)
+    foundversion >= version
+end
+
+libspatialindex = library_dependency("libspatialindex",
+    aliases=["libspatialindex_c", "spatialindex_c-64", "spatialindex_c"],
+    validate=version_check)
+
+if is_unix()
+    url = "http://download.osgeo.org/libspatialindex/"
+    spatialindexname = "spatialindex-src-$version"
+    srcdir = joinpath(BinDeps.depsdir(libspatialindex), "src", spatialindexname)
+    usrdir = joinpath(BinDeps.depsdir(libspatialindex), "usr")
+    provides(Sources, URI("$url$spatialindexname.tar.gz"),
+             libspatialindex, os=:Unix)
+    provides(SimpleBuild,
+        (@build_steps begin
+            GetSources(libspatialindex)
+            @build_steps begin
+                ChangeDirectory(srcdir)
+                `./configure --prefix=$usrdir`
+                `make`
+                `make install`
+            end
+        end), libspatialindex, os=:Unix)
+end
+
+if is_windows()
+    url = "http://download.osgeo.org/libspatialindex/"
+    archive = "libspatialindex-$version-win-msvc-2013-x64.zip"
+    uri = URI("$url$archive")
+    libpath = joinpath(BinDeps.depsdir(libspatialindex), "libspatialindex", "build", "bin")
+    provides(Binaries, uri, libspatialindex,
+        unpacked_dir=".", installed_libpath=libpath, os=:Windows)
+end
+
+@BinDeps.install Dict(:libspatialindex => :libspatialindex)
--- src/capi.jl
+++ src/capi.jl
@@ -1,6 +1,11 @@
 module C
 
-    const libspatialindex = "libspatialindex_c"
+    const depfile = joinpath(@__DIR__, "..", "deps", "deps.jl")
+    if isfile(depfile)
+        include(depfile)
+    else
+        error("libspatialindex not properly installed. Please run Pkg.build(\"LibSpatialIndex\")")
+    end
 
     include("include/sidx_config.jl")
     include("include/sidx_api.jl")
