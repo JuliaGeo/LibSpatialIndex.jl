4d286c96d59486cdf3777d1296d4d1311de7e833
jaakkor2 <jaakkor2@users.noreply.github.com>
2020-04-18 07:28:25 +0200

    Update to use jll artifact

--- src/capi.jl
+++ src/capi.jl
@@ -1,11 +1,6 @@
 module C
 
-    const depfile = joinpath(@__DIR__, "..", "deps", "deps.jl")
-    if isfile(depfile)
-        include(depfile)
-    else
-        error("libspatialindex not properly installed. Please run Pkg.build(\"LibSpatialIndex\")")
-    end
+    using LibSpatialIndex_jll
 
     include("include/sidx_config.jl")
     include("include/sidx_api.jl")
